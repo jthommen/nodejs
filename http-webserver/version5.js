d366165650543c6607a1423707d9cfcfe15433f6
Juri <thommenju@gmail.com>
2017-09-26 23:02:03 +0100

    Add body-parser

--- http-webserver/version5.js
+++ http-webserver/version5.js
@@ -1,5 +1,10 @@
 var express = require('express');
 
+// Middleware to parse POST request bodies
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonParser = bodyParser.json();
+
 // Create express app that returns function
 var app = express();
 
@@ -25,7 +30,20 @@ app.get('/', function(req, res){
 
 // Dynamic routes with pattern matching & params
 app.get('/person/:id', function(req, res){
-    res.render('person', { ID: req.params.id });
+    res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+// Form via post & bodyparser
+app.post('/person', urlencodedParser, function(req, res){
+    res.send('Thank you!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);    
+});
+
+app.post('/personjson', jsonParser, function(req, res){
+    res.send('Thank you for the JSON data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);  
 });
 
 app.get('/json', function(req, res){
